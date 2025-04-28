94c9d685ea9eee734464b23c22a014b305c876bf
Abhishek <127682284+ByAbhishekCode@users.noreply.github.com>
2025-04-28 03:25:17 +0000

    convertion

---
+++ 01_basics/03_convertiondatatype.js
@@ -0,0 +1,9 @@
+// let score = "Abhishek";
+let score = "33";
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+let isLoggedIn = " ";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+console.log(booleanIsLoggedIn);
