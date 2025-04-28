b6f7f589342399faaf06f1cfc6f15843e8ead18d
Abhishek <127682284+ByAbhishekCode@users.noreply.github.com>
2025-04-28 03:08:32 +0000

    variable understading

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 12345;
+let accountEmail = "abhikumar@gmail.com";
+var accountPassward = "newpapagame";
+accountCity = "kanpur";
+const ram = null;
+var newId = 12
+console.table([typeof accountId, typeof accountEmail, typeof accountPassward, typeof accountCity, typeof ram,typeof newId])
+
+console.table([ accountId,  accountEmail,  accountPassward,  accountCity,ram,newId])
