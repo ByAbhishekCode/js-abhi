35519694cab0c66a4a28b97abefc19e653ac50d4
Abhishek <127682284+ByAbhishekCode@users.noreply.github.com>
2025-04-28 09:27:33 +0000

    string and its function

---
+++ 01_basics/05_string.js
@@ -0,0 +1,5 @@
+let string = new String("I love India & love Indian People");
+let city = "Kanpur";
+let cityPeopleNumber = 500000
+console.log(`this is my city ${city} and number of popele is ${cityPeopleNumber}.`)
+console.log(string);
