1228b3cac64c4125de44f6a7e4e295d137f7ea91
Abhishek <127682284+ByAbhishekCode@users.noreply.github.com>
2025-04-28 03:16:19 +0000

    Data Type

---
+++ 01_basics/01_datatype.js
@@ -0,0 +1,8 @@
+let name = "abhishek";
+let name2 = " ";
+let name3 = "";
+let boolean = 1;
+let boolean1 = true;
+console.table([typeof name,typeof name2,typeof name3,typeof boolean,typeof boolean1]);
+console.table([ name, name2, name3, boolean, boolean1]);
+console.table([typeof Boolean,typeof Object,typeof null,typeof undefined]);
