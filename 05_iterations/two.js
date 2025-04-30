d572546bd336ede2a8ea398aa4825275cd73380f
Abhishek <127682284+ByAbhishekCode@users.noreply.github.com>
2025-04-30 06:00:31 +0000

    while and do while understanding

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// let index = 1
+// while(index<=10){
+//     console.log(`value of index is ${index}`)
+//     index = index+1
+// }
+
+let myArray =['flash','batman','superman']
+
+let arry = 0;
+while(arry < myArray.length){
+    console.log(`value of index is ${myArray[arry]}`);
+    arry = arry+1
+    
+}
+let socore = 1
+do {
+    console.log(`score is ${socore}`);
+    socore++
+    
+} while (socore<=10);
